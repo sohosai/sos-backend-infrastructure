2eeb570e39414a205eaf732d0df8513a128d6f0a
coord.e <me@coord-e.com>
2021-04-20 09:36:21 +0000

    Change: Enable TLS using proxied DNS record

--- dns_record.tf
+++ dns_record.tf
@@ -1,6 +1,16 @@
+# 'proxied' records for free TLS (HTTPS) and direct records for deployments via SSH
+
 resource "cloudflare_record" "beta" {
   zone_id = var.cloudflare_zone_id
-  name    = "api.beta.online"
+  name    = "online-api-beta"
+  value   = local.beta_ip_address
+  type    = "A"
+  proxied = true
+}
+
+resource "cloudflare_record" "beta_direct" {
+  zone_id = var.cloudflare_zone_id
+  name    = "direct.online-api-beta"
   value   = local.beta_ip_address
   type    = "A"
   # value of 1 is 'automatic'
@@ -10,7 +20,15 @@ resource "cloudflare_record" "beta" {
 
 resource "cloudflare_record" "nightly" {
   zone_id = var.cloudflare_zone_id
-  name    = "api.nightly.online"
+  name    = "online-api-nightly"
+  value   = local.nightly_ip_address
+  type    = "A"
+  proxied = true
+}
+
+resource "cloudflare_record" "nightly_direct" {
+  zone_id = var.cloudflare_zone_id
+  name    = "direct.online-api-nightly"
   value   = local.nightly_ip_address
   type    = "A"
   # value of 1 is 'automatic'
